220f09b6e3b951cb293133342f23fddc13f4796c
Pi Lanningham <pi@sundaeswap.finance>
2023-01-21 23:57:13 +0000

    Add pagination support to the command line

--- cmd/kugo/main.go
+++ cmd/kugo/main.go
@@ -21,6 +21,12 @@ var opts struct {
 	PolicyID     string
 	AssetName    string
 	AssetNameHex string
+
+	CreatedBefore uint64
+	CreatedAfter  uint64
+	SpentBefore   uint64
+	SpentAfter    uint64
+	Overlapping   uint64
 }
 
 func main() {
@@ -70,6 +76,37 @@ func main() {
 			EnvVars:     []string{"ASSET_NAME_HEX"},
 			Destination: &opts.AssetNameHex,
 		},
+
+		&cli.Uint64Flag{
+			Name:        "created-before",
+			Usage:       "Only print UTXOs that were created before a specific slot",
+			EnvVars:     []string{"CREATED_BEFORE"},
+			Destination: &opts.CreatedBefore,
+		},
+		&cli.Uint64Flag{
+			Name:        "created-after",
+			Usage:       "Only print UTXOs that were created after a specific slot",
+			EnvVars:     []string{"CREATED_AFTER"},
+			Destination: &opts.CreatedAfter,
+		},
+		&cli.Uint64Flag{
+			Name:        "spent-before",
+			Usage:       "Only print UTXOs that were spent, and spent before a specific slot",
+			EnvVars:     []string{"SPENT_BEFORE"},
+			Destination: &opts.SpentBefore,
+		},
+		&cli.Uint64Flag{
+			Name:        "spent-after",
+			Usage:       "Only print UTXOs that were spent, and spent after a specific slot",
+			EnvVars:     []string{"SPENT_AFTER"},
+			Destination: &opts.SpentAfter,
+		},
+		&cli.Uint64Flag{
+			Name:        "overlapping",
+			Usage:       "Only print UTXOs that 'overlap' a specific slot, i.e. were created before, or spent after",
+			EnvVars:     []string{"OVERLAPPING"},
+			Destination: &opts.Overlapping,
+		},
 	}
 	app.Action = action
 	err := app.Run(os.Args)
@@ -86,7 +123,7 @@ func action(_ *cli.Context) error {
 		kugo.WithEndpoint(opts.Endpoint),
 	)
 
-	var filters []kugo.Filter
+	var filters []kugo.MatchesFilter
 	if opts.Unspent {
 		filters = append(filters, kugo.OnlyUnspent())
 	} else if opts.Spent {
@@ -107,6 +144,21 @@ func action(_ *cli.Context) error {
 			assetName = string(assetNameBytes)
 		}
 		filters = append(filters, kugo.AssetID(chainsync.AssetID(fmt.Sprintf("%v.%v", opts.PolicyID, assetName))))
+	}
+	if opts.Overlapping > 0 {
+		filters = append(filters, kugo.Overlapping(opts.Overlapping))
+	}
+
+	if opts.CreatedBefore > 0 {
+		filters = append(filters, kugo.CreatedBefore(opts.CreatedBefore))
+	} else if opts.CreatedAfter > 0 {
+		filters = append(filters, kugo.CreatedAfter(opts.CreatedAfter))
+	}
+
+	if opts.SpentBefore > 0 {
+		filters = append(filters, kugo.CreatedBefore(opts.SpentBefore))
+	} else if opts.SpentAfter > 0 {
+		filters = append(filters, kugo.CreatedAfter(opts.SpentAfter))
 	}
 
 	matches, err := client.Matches(ctx, filters...)
